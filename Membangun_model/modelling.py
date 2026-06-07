9c9e34b45c0cf1960c2c30a86f1900cfc93da7a3
titiwpramesti26 <titialfnprmst@mhs.unsiq.ac.id>
2026-06-07 15:31:15 +0700

    menambahkan mlflow autolog

--- Membangun_model/modelling.py
+++ Membangun_model/modelling.py
@@ -4,6 +4,7 @@ import numpy as np
 import dagshub
 import mlflow
 import mlflow.sklearn
+import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, classification_report
@@ -28,7 +29,8 @@ y = df['Outcome']
 # 4. Train-Test Split (80:20)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
-# 5. Menjalankan Pelatihan Model Dasar dengan Manual Logging
+# 5. Menjalankan Pelatihan Model Dasar dengan Manual Logging dan Auto Logging
+mlflow.autolog()  # Aktifkan Auto Logging untuk semua parameter, metrik, dan model
 with mlflow.start_run(run_name="Baseline_Model_RF"):
     # Definisi parameter dasar
     n_estimators = 100
